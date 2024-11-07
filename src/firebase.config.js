a09349d3e6a3fc10fcd4fe1dc07e55d0129d35a5
clouddrop7 <cloud@clouddropdesigns.com>
2024-11-06 23:50:21 -0800

    changing to .env file for testing

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,16 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+
+// Use environment variables from .env file
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBLTg6XNoYwqst424IEAq820c3w2fSeRiA",
-  authDomain: "cloudportfolio-440621.firebaseapp.com",
-  projectId: "cloudportfolio-440621",
-  storageBucket: "cloudportfolio-440621.firebasestorage.app",
-  messagingSenderId: "1092386547852",
-  appId: "1:1092386547852:web:efc52075b59923151433a9",
-  measurementId: "G-BS59WYVLWG"
-};
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
+  appId:process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+}
+
+export default firebaseConfig;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app); // Export Firestore instance
